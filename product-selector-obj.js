9131801767d2962ff7dadf2c3e690d1763a454b3
Zachary Porter <zak@codebeagle.com>
2022-01-08 18:51:54 -0700

    Initial Commit
    Contains all HTML, CSS, and JS up to 1/8/22 in preparation for Netlify staging site.

---
+++ product-selector-obj.js
@@ -0,0 +1,170 @@
+var productSelector = {
+    products: {
+        'dom-analog': {
+            'available': false,
+            'message': 'Coming Soon!'
+        },
+        'dom-dou-dz': {
+            'available': true,
+            'modelImage': '/images/dom_dou_sz.jpg',
+            'modelTitle': 'Dometic DOU SZ',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-350-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-350-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 350',
+        },
+        'dom-ct': {
+            'available': true,
+            'modelImage': '/images/dom-ct.jpg',
+            'modelTitle': 'Dometic CT',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-351-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-351-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 351',
+        },
+        'dom-sz-lcd': {
+            'available': true,
+            'modelImage': '/images/dom_sz_lcd.jpg',
+            'modelTitle': 'Dometic SZ LCD',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-351-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-351-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 351',
+        },
+        'dom-cc-dt': {
+            'available': true,
+            'modelImage': '/images/dom_cc_dt.jpg',
+            'modelTitle': 'Dometic CC DT',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-357-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-357-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 357',
+        },
+        'cole-9000-zc': {
+            'available': true,
+            'modelImage': '/images/cole_9000-zc.jpg',
+            'modelTitle': 'Coleman 9000 ZC',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [
+                {
+                    'connection': 'clip',
+                    'label': 'Clip',
+                    'model': 'EasyTouch RV 355'
+                },
+                {
+                    'connection': 'wired',
+                    'label': 'Wired',
+                    'model': 'EasyTouch RV 355C'
+                }
+            ],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-355-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-355-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 355',
+        },
+        'cole-tslide-12vdc': {
+            'available': true,
+            'modelImage': '/images/cole_tslide_12vdc.jpg',
+            'modelTitle': 'Dometic TSLIDE 12VDC',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-352-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-352-X02'
+                }
+            ],
+            'resultModel': 'EasyTouch RV 352',
+        },
+        'cole-mzzc8000-horz': {
+            'available': true,
+            'modelImage': '/images/cole_mzzc8000-horz.jpg',
+            'modelTitle': 'Coleman MZZC8000 HORZ',
+            'modelDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie quis faucibus magna sagittis neque. Enim, suscipit nunc, interdum at bibendum duis et duis nibh.',
+            'connections': [
+                {
+                    'connection': 'clip',
+                    'label': 'Clip',
+                    'model': 'EasyTouch RV 354'
+                },
+                {
+                    'connection': 'wired',
+                    'label': 'Wired',
+                    'model': 'EasyTouch RV 354C'
+                }
+            ],
+            'colorways': [
+                {
+                    'color': 'black',
+                    'label': 'Black',
+                    'part': 'ASY-354-X01'
+                },
+                {
+                    'color': 'white',
+                    'label': 'White',
+                    'part': 'ASY-354-X02'
+                }
+            ],
+            'resultModel': '',
+        },
+    }
+};
---
+++ product-selector.js
@@ -0,0 +1,173 @@
+$(window).ready(function() {
+    //--- VAR
+    var theResult = [
+        '', // Model Name
+        '', // Part Number
+        '', // isWired Suffix
+        '' // Color
+    ]
+    var theModal = $('#js-modal');
+    var closeModalBtn = $('#js-close-modal');
+    var items = $('.item');
+    var selectedModel = '';
+    var stepOne = $('#js-modal-step-1');
+    var stepTwo = $('#js-modal-step-2');
+    var stepResults = $('#js-modal-step-results');
+    var resultsBtn = $('#js-results-btn');
+    var isWired = false;
+    var backBtn = $('.js-back-button');
+
+
+
+    //--- FUNCTIONS
+    function closeModal() {
+        theModal.addClass('hidden');
+    }
+
+    function selectItem(e) {
+        e.preventDefault();
+        selectedModel = $(this).data('item');
+
+        // Call slide two and pass in selected model
+        slideTwo(selectedModel);
+    }
+
+    function slideTwo(model) {
+        // Check if model exists in data object
+        if (productSelector.products[model]) {
+
+            // Set step 2 model info
+            stepTwo.find('#js-model-image').attr('src',productSelector.products[model].modelImage);
+            stepTwo.find('#js-model-title').html(productSelector.products[model].modelTitle);
+            stepTwo.find('#js-model-description').html(productSelector.products[model].modelDescription);
+
+            // Set step 2 colorway info
+            var theConnections = productSelector.products[model].connections;
+            if (theConnections.length > 0) {
+                console.log('there are connections!')
+                stepTwo.find('#js-connection-options').removeClass('hidden');
+                
+                for (var i = 0; i < theConnections.length; i++) {
+                    var theConnection = '\
+                        <div class="connection js-connection" data-connection="' + theConnections[i].connection +'" data-model="' + theConnections[i].model +'" data-label="' + theConnections[i].label +'">\
+                            <div class="swatch ' + theConnections[i].connection + '">\
+                                <div class="checkmark">\
+                                    <img src="/images/checkmark.svg" alt="checkmark">\
+                                </div>';
+                    switch (theConnections[i].connection) {
+                        case 'clip':
+                            theConnection += '<img src="/images/etrv_ma_harness.jpg" width="166" height="85" alt="clip connection">';
+                            break;
+
+                        case 'wired':
+                            theConnection += '<img src="/images/connectors-wire-nuts.jpg" width="166" height="85" alt="wired connection"></img>';
+                            break;
+                        
+                        default:
+                            theConnection += '';
+                    }
+                    theConnection += '\
+                             </div>\
+                            <p>' + theConnections[i].label + '</p>\
+                        </div>';
+                    stepTwo.find('#js-connections').append(theConnection);
+                }
+            }
+
+
+            // Set step 2 colorway info
+            var theColorways = productSelector.products[model].colorways;
+            if (theColorways.length > 0) {
+                console.log('there are colors!')
+                stepTwo.find('#js-colorways').removeClass('hidden');
+                
+                for (var i = 0; i < theColorways.length; i++) {
+                    var theColor = '\
+                        <div class="color js-color"  data-part="' + theColorways[i].part +'" data-label="' + theColorways[i].label +'">\
+                            <div class="swatch ' + theColorways[i].color + '">\
+                                <div class="checkmark">\
+                                    <img src="/images/checkmark.svg" alt="checkmark">\
+                                </div>\
+                            </div>\
+                            <p>' + theColorways[i].label + '</p>\
+                        </div>';
+                    stepTwo.find('#js-colors').append(theColor);
+                }
+            }
+
+            // Set results model
+            theResult[0] = productSelector.products[model].resultModel;
+
+            // Close step 1 and open step 2
+            stepOne.addClass('hidden');
+            stepTwo.removeClass('hidden');
+
+        } else {
+            console.log('no item data');
+        }
+    }
+
+    function setConnection() {
+        console.log('connection clicked');
+        $('.connections .swatch').removeClass('selected');
+        $(this).find('.swatch').addClass('selected');
+
+        // Set global wired variable if true;
+        if ( $(this).data('connection') === 'wired') {
+            isWired = true;
+            theResult[2] = "-C";
+        } else {
+            isWired = false;
+            theResult[2] = "";
+        }
+
+        theResult[0] = $(this).data('model');
+        console.log(theResult,isWired);
+    }
+
+    function setColor() {
+        console.log('color clicked');
+        $('.colors .swatch').removeClass('selected');
+        $(this).find('.swatch').addClass('selected');
+        theResult[1] = $(this).data('part');
+        theResult[3] = $(this).data('label');
+        console.log(theResult);
+    }
+
+    function slideResults() {
+        // Set results slide info
+        stepResults.find('#js-result-model').html(theResult[0]);
+        stepResults.find('#js-result-part').html('Part Number: ' + theResult[1] + theResult[2]);
+        stepResults.find('#js-result-color').html('Color: ' + theResult[3]);
+
+        //Close step 2 and open results
+        stepTwo.addClass('hidden');
+        stepResults.removeClass('hidden');
+    }
+
+    function reset() {
+        // Hide all slides and display the first slide
+        stepOne.removeClass('hidden');
+        stepTwo.addClass('hidden');
+        stepResults.addClass('hidden');
+
+        // Empty result var
+        theResult = ["","","",""];
+
+        // Empty the connections and colors
+        stepTwo.find('#js-connection-options').addClass('hidden');
+        stepTwo.find('#js-connections').html('');
+        stepTwo.find('#js-colorways').addClass('hidden');
+        stepTwo.find('#js-colors').html('');
+    }
+
+
+    //--- INIT
+    closeModalBtn.on('click', closeModal);
+    items.on('click', selectItem);
+    resultsBtn.on('click', slideResults);
+    backBtn.on('click', reset);
+    $(document).on('click', '.js-color', setColor);
+    $(document).on('click', '.js-connection', setConnection);
+
+});
